0f20cfc5252db7f0b83ef3394b1d892240a11126
hariidev <hsugeen14@gmail.com>
2025-04-24 11:28:09 +0530

    Add basic wallet storage functionality using JSON file

--- wallet.php
+++ wallet.php
@@ -1,1 +1,16 @@
+<?php
 
+$storageFile = 'wallet.json';
+
+function loadWallet() {
+    global $storageFile;
+    if (!file_exists($storageFile)) {
+        file_put_contents($storageFile, json_encode([]));
+    }
+    return json_decode(file_get_contents($storageFile), true);
+}
+
+function saveWallet($data) {
+    global $storageFile;
+    file_put_contents($storageFile, json_encode($data));
+}
